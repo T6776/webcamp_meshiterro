97cf492d06d600625bf68a5107b28847dfd859d5
T6776 <t930324@gmail.com>
2021-10-16 11:16:51 +0000

    [Finish} 4-4

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'e571d81f0b5ebfa9f40af5f7bc0978b94a968504ec75c5b5b23af9e3806ee1a3fbb1f7c565af285ca9a4c3667b0a7311682ef4467429d3bad3fedac46111d203'
